e237e6c1539239c68be11a281b8c0f3dfd0b987b
Srikar Gouru <srikarg89@gmail.com>
2025-12-04 19:23:42 -0500

    Update images

--- scripts/draw_map_state.py
+++ scripts/draw_map_state.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.colors
 import matplotlib.patheffects as pe
+import matplotlib.patches as patches
 
 # cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","darkcyan","orange","gray","black"])
 # cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","gray","gray","gray","black"])
@@ -34,20 +35,20 @@ import matplotlib.patheffects as pe
 
 # Cell / Path domination maps
 
-# cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","purple","yellow","black"])
-# map = [
-#     [1, 1, 1, 3/4, 3/4, 3/4, 3/4],
-#     [1, 1, 0, 0, 1, 3/4, 3/4],
-#     [1, 1, 0, 1, 1, 1, 1],
-#     [1/4, 0, 0, 1, 1, 1, 1],
-# ]
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","darkorchid","yellow","black"])
+map = [
+    [1, 1, 1, 3/4, 3/4, 3/4, 2/4],
+    [1, 1, 0, 0, 1, 3/4, 3/4],
+    [1, 1, 0, 1, 1, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1],
+]
 
-# map = [
-#     [1, 1, 1, 3/4, 3/4, 3/4, 3/4],
-#     [1, 1, 3/4, 0, 1, 3/4, 3/4],
-#     [1, 1, 0, 1, 1, 1, 1],
-#     [1/4, 0, 0, 1, 1, 1, 1],
-# ]
+map = [
+    [1, 1, 1, 3/4, 3/4, 2/4, 3/4],
+    [1, 1, 3/4, 0, 1, 3/4, 3/4],
+    [1, 1, 0, 1, 1, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1],
+]
 
 # map = [
 #     [1, 1, 1, 0, 0, 0, 2/4],
@@ -66,27 +67,53 @@ import matplotlib.patheffects as pe
 #     [1/3, 1, 0, 0, 0, 0, 0],
 # ]
 
-# Pivot pruning
-cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","gray","firebrick","yellow","black"])
-map = [
-    [1/5, 2/5, 2/5, 2/5, 2/5, 2/5],
-    [1, 1, 1, 1, 1, 4/5],
-    [1, 4/5, 4/5, 3/5, 1, 4/5],
-    [1, 0, 1, 1, 1, 4/5],
-    [1, 4/5, 4/5, 4/5, 4/5, 3/5],
-]
-
+# # Pivot pruning
+# cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","gray","firebrick","yellow","black"])
 # map = [
 #     [1/5, 2/5, 2/5, 2/5, 2/5, 2/5],
-#     [1, 1, 1, 1, 1, 0],
-#     [1, 4/5, 4/5, 3/5, 1, 0],
-#     [1, 0, 1, 1, 1, 0],
-#     [1, 0, 0, 0, 0, 0],
+#     [1, 1, 1, 1, 1, 4/5],
+#     [1, 4/5, 4/5, 3/5, 1, 4/5],
+#     [1, 0, 1, 1, 1, 4/5],
+#     [1, 4/5, 4/5, 4/5, 4/5, 3/5],
 # ]
 
+# # map = [
+# #     [1/5, 2/5, 2/5, 2/5, 2/5, 2/5],
+# #     [1, 1, 1, 1, 1, 0],
+# #     [1, 4/5, 4/5, 3/5, 1, 0],
+# #     [1, 0, 1, 1, 1, 0],
+# #     [1, 0, 0, 0, 0, 0],
+# # ]
+
+# BresLOS
+# cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","gray","black"])
+# map = [
+#     [2/3, 2/3, 2/3, 2/3, 2/3],
+#     [1, 1, 1/3, 1, 1],
+#     [2/3, 2/3, 2/3, 2/3, 2/3],
+#     [2/3, 1, 1, 1, 2/3],
+#     [0, 1, 0, 0, 0],
+# ]
+
+# Movement
+# cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green"])
+# map = [
+#     [0, 0, 0],
+#     [0, 1, 0],
+#     [0, 0, 0],
+# ]
+
+# Heuristic
+# cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green","gray","red","yellow","black"])
+# map = [
+#     [4/5, 4/5, 4/5, 3/5],
+#     [0, 1, 1, 1],
+#     [4/5, 4/5, 4/5, 3/5],
+#     [0, 1, 1, 1],
+#     [2/5, 2/5, 2/5, 1/5],
+# ]
 
 fig, ax = plt.subplots()
-
 
 im = ax.imshow(map, cmap=cmap)
 
@@ -96,6 +123,38 @@ ax.set_yticks(np.arange(-.5, len(map), 1))
 
 ax.set_yticklabels([])
 ax.set_xticklabels([])
+
+# pairs = [
+#     ((1,1), (1,0)),
+#     ((1,1), (0,1)),
+#     ((1,1), (1,2)),
+#     ((1,1), (2,1)),
+# ]
+
+# for prev_pos, after_pos in pairs:
+#     major_offset, minor_offset, length, width = 0.5, 0.0, 1.0, 0.4
+#     diag_minor_offset = 0.2
+#     diag_length = .75
+
+#     if after_pos == (prev_pos[0] + 1, prev_pos[1]): # Right
+#         arrow = patches.Arrow(prev_pos[0] + minor_offset, prev_pos[1] + minor_offset, length, 0, width=width, color='black')
+#     elif after_pos == (prev_pos[0] - 1, prev_pos[1]): # Left
+#         arrow = patches.Arrow(prev_pos[0] - minor_offset, prev_pos[1] - minor_offset, -length, 0, width=width, color='black')
+#     elif after_pos == (prev_pos[0], prev_pos[1] + 1): # Down
+#         arrow = patches.Arrow(prev_pos[0] - minor_offset, prev_pos[1] + minor_offset, 0, length, width=width, color='black')
+#     elif after_pos == (prev_pos[0], prev_pos[1] - 1): # Up
+#         arrow = patches.Arrow(prev_pos[0] + minor_offset, prev_pos[1] - minor_offset, 0, -length, width=width, color='black')
+#     elif after_pos == (prev_pos[0] + 1, prev_pos[1] + 1): # Down-Right
+#         arrow = patches.Arrow(prev_pos[0], prev_pos[1] + diag_minor_offset, diag_length, diag_length - diag_minor_offset, width=width, color='green')
+#     elif after_pos == (prev_pos[0] - 1, prev_pos[1] + 1): # Down-Left
+#         arrow = patches.Arrow(prev_pos[0] - diag_minor_offset, prev_pos[1], -diag_length + diag_minor_offset, diag_length, width=width, color='green')
+#     elif after_pos == (prev_pos[0] + 1, prev_pos[1] - 1): # Up-Right
+#         arrow = patches.Arrow(prev_pos[0] + diag_minor_offset, prev_pos[1], diag_length - diag_minor_offset, -diag_length, width=width, color='green')
+#     elif after_pos == (prev_pos[0] - 1, prev_pos[1] - 1): # Up-Left
+#         arrow = patches.Arrow(prev_pos[0], prev_pos[1] - diag_minor_offset, -diag_length, -diag_length + diag_minor_offset, width=width, color='green')
+    
+#     arrow.set_zorder(5)
+#     ax.add_patch(arrow)
 
 # plt.text(
 #     5.75, 0.2, "s1",
@@ -108,4 +167,5 @@ ax.set_xticklabels([])
 #     ]
 # )
 
+plt.savefig('../images/ExCellDom2.png', dpi=500, bbox_inches='tight')
 plt.show()
